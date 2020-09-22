938d3ce0681a3ce1aae2dd8d602cb2406109dd92
repl.it user <replituser@example.com>
2020-09-22 20:57:16 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -92,7 +92,7 @@ void * searchMap(HashMap * map,  char * key)
   long pos = hash(key, map->current); 
 
   //resolucion de colisiones
-  while((map->buckets[pos] != NULL) && (is_equal(map->buckets[pos]->key, key) != 1)) pos++;
+  while((map->buckets[pos] != NULL) && (is_equal(map->buckets[pos]->key, key) != 0)) pos++;
 
   if(map->buckets[pos] == NULL) return NULL;
 
