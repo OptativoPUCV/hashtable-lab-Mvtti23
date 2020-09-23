554b862f4fe9c4522f0634ce9b179fc1c4c3bc73
repl.it user <replituser@example.com>
2020-09-23 19:37:20 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -46,17 +46,7 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value)
 {
-  long pos = hash(key, map->capacity);
-
-  //resolucion de colisiones
-  while(map->buckets[pos] != NULL) pos++;
-
-  if(is_equal(map->buckets[pos]->key, key) == 1) pos++;
-
- //ingreso del bucket nuevo
-  map->buckets[pos] = createPair(key, value);
-  map->current = pos;
-  map->size += 1;
+  
 }
 
 void enlarge(HashMap * map) {
