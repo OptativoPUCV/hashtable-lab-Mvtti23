bde205ce121580ea7ffc794411aaa4b2773acc21
repl.it user <replituser@example.com>
2020-09-24 23:25:07 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -126,7 +126,7 @@ void eraseMap(HashMap * map,  char * key)
 
 void * searchMap(HashMap * map,  char * key)
 { 
-  /*
+  
   long pos = hash(key, map->capacity);
 
   while(is_equal(map->buckets[pos]->key, key) == 0) pos++;
@@ -137,8 +137,7 @@ void * searchMap(HashMap * map,  char * key)
   map->current = pos;
 
   return map->buckets[pos]->value;
-  */
-  return NULL;
+  
 }
 
 void * firstMap(HashMap * map) 
